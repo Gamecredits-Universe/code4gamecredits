632fd3b84d645d299780fa32c941d39c2b4edb56
Michael Witrant <michael@ouvrages-web.fr>
2014-02-15 08:39:10 +0100

    btc_human returns nil on nil amount

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   def btc_human amount, options = {}
+    return nil unless amount
     nobr = options.has_key?(:nobr) ? options[:nobr] : true
     currency = options[:currency] || false
     btc = "%.8f Ᵽ" % to_btc(amount)
@@ -9,6 +10,6 @@ module ApplicationHelper
   end
 
   def to_btc satoshies
-    satoshies.to_d / PeercoinBalanceUpdater::COIN
+    satoshies.to_d / PeercoinBalanceUpdater::COIN if satoshies
   end
 end
