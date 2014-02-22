2e5cfa14a8de264e0216e8c967802757cbc1f29b
aditya-kapoor <aditya.kapoor@vinsol.com>
2014-02-23 02:30:35 +0530

    use new ruby syntax and remove find in favor for where

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   before_action except: [:login, :index] do
-    @user = User.find params[:id]
+    @user = User.where(id: params[:id]).first
     unless current_user && current_user == @user
       redirect_to root_path
     end
@@ -11,11 +11,11 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.order(:withdrawn_amount => :desc, :commits_count => :desc).where('commits_count > 0').page(params[:page]).per(30)
+    @users = User.order(withdrawn_amount: :desc, commits_count: :desc).where('commits_count > 0').page(params[:page]).per(30)
   end
 
   def update
-    if @user.update_attributes(users_params)
+    if @user.update(users_params)
       redirect_to @user, notice: 'Your information saved!'
     else
       render :show, alert: 'Error updating peercoin address'
@@ -23,13 +23,13 @@ class UsersController < ApplicationController
   end
 
   def login
-    @user = User.find_by(login_token: params[:token])
+    @user = User.where(login_token: params[:token]).first
     if @user
-      sign_in_and_redirect @user, :event => :authentication
       if params[:unsubscribe]
         @user.update unsubscribed: true
         flash[:alert] = 'You unsubscribed! Sorry for bothering you. Although, you still can leave us your peercoin address to get your tips.'
       end
+      sign_in_and_redirect @user, event: :authentication
     else
       redirect_to root_url, alert: 'User not found'
     end
