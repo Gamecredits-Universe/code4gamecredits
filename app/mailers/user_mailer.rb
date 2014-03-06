e56b8b0bbfe7821ae7f44cb52ac586c928e4bca4
Michael Witrant <michael@ouvrages-web.fr>
2014-03-06 20:39:26 +0100

    send security issue email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,9 @@ class UserMailer < ActionMailer::Base
 
     mail to: user.email, subject: "You received a tip for your commit"
   end
+
+  def security_issue(user)
+    @user = user
+    mail to: user.email, subject: "Security issue on peer4commit.com"
+  end
 end
