d7eb68c0cee0d328db3f47f51c8d15f2e63ece56
Michael Witrant <michael@ouvrages-web.fr>
2014-05-30 17:11:48 +0200

    Use poltergeist as test browser when javascript is needed

--- Gemfile
+++ Gemfile
@@ -82,4 +82,5 @@ group :test do
   gem 'database_cleaner'
   gem 'rspec-rails'
   gem 'factory_girl_rails'
+  gem 'poltergeist'
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -56,3 +56,12 @@ end
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
 
+require 'capybara/poltergeist'
+if ENV["FIREFOX"]
+  Capybara.javascript_driver = :selenium
+else
+  Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(app, inspector: true)
+  end
+  Capybara.javascript_driver = :poltergeist
+end
