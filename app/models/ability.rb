b445e781efc17b3aea3c758d948b224e1e32bf94
Michael Witrant <michael@ouvrages-web.fr>
2014-05-31 10:48:20 +0200

    Everyone can see projects and distributions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,10 +2,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    can :read, Project
+    can :read, Distribution
+
     if user and user.nickname.present?
       can [:update, :decide_tip_amounts], Project, collaborators: {login: user.nickname}
-      can [:create, :read], Project
-      can [:create, :recipient_suggestions, :read, :send_transaction], Distribution, project: {collaborators: {login: user.nickname}}
+      can [:create], Project
+      can [:create, :recipient_suggestions, :send_transaction], Distribution, project: {collaborators: {login: user.nickname}}
     end
   end
 end
