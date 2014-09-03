b892ed299adb864c1f0b6c5b050980dce294d692
Michael Witrant <michael@ouvrages-web.fr>
2014-09-03 07:54:50 +0200

    handle new github error

--- app/models/project.rb
+++ app/models/project.rb
@@ -44,7 +44,8 @@ class Project < ActiveRecord::Base
         client.commits(full_name)
       end
     rescue Octokit::BadGateway, Octokit::NotFound, Octokit::InternalServerError,
-           Errno::ETIMEDOUT, Faraday::Error::ConnectionFailed, Octokit::Forbidden => e
+           Errno::ETIMEDOUT, Faraday::Error::ConnectionFailed, Octokit::Forbidden,
+           Octokit::Conflict => e
       Rails.logger.info "Project ##{id}: #{e.class} happened"
     rescue StandardError => e
       if CONFIG["airbrake"]
